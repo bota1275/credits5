a3ed31aa02080a9b63d4dc62b1aea3e049258edf
bota1275 <68181846+bota1275@users.noreply.github.com>
2020-07-13 00:54:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['ID'];
+const myid = ['731466812679782430'];
 const prefix = ['$'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
